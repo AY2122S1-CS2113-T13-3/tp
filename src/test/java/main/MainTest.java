adc21224e03dc4e1ea44e8f13f3cb34a90b2715e
LAPTOP-V2NPSHUB\19656 <1965637476@qq.com>
2021-10-20 09:34:45 +0800

    minor change

--- src/test/java/main/MainTest.java
+++ src/test/java/main/MainTest.java
@@ -1,18 +1,14 @@
-package seedu.duke;
+package main;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-class DukeTest {
+class MainTest {
     @Test
     public void sampleTest() {
         assertTrue(true);
     }
 
-    @Test
-    void checkEchoFunction() {
-        assertEquals("Test Message", seedu.duke.Duke.echoMessage("Test Message"));
-    }
 }
